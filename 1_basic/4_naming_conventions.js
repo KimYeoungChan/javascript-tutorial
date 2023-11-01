3514203b59f1f23343be2abf3acbbabf565c4ee2
Channy <69968484+KimYeoungChan@users.noreply.github.com>
2023-11-02 00:56:22 +0900

    1_basic

---
+++ 1_basic/4_naming_conventions.js
@@ -0,0 +1,32 @@
+/**
+ * Naming Conventions
+ *
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다.
+ *    1Name, 2Helllo
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ *    var const = 'var';
+ */
+
+let codeFactory = "코드팩토리";
+var $ive = "아비브";
+const _yujin = "안유진";
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yujin);
+
+// let 1name = 'no'
+// let const = 'attention'
+
+/**
+ * Naming Convention 2
+ *
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ * 2) snake_case -> 파이썬같은 언어에서 사용
+ * 3) PascalCase -> C# 마이크로소프트 계열의 언어에서 사용함
+ */
+// 안유진
+const anYuJin = "안유진";
+console.log(anYuJin);
---
+++ 1_basic/6_hoisting.js
@@ -0,0 +1,22 @@
+/**
+ * Hoisting
+ */
+console.log("Hello");
+console.log("World");
+
+// console.log(name);
+// var name = "코트팩토리";
+// console.log(name);
+
+/**
+ * Hoistring은 무엇인가?
+ *
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
+ */
+// var name;
+// console.log(name);
+// var name = "코트팩토리";
+// console.log(name);
+
+console.log(yuJin);
+const yuJin = "안유진";
